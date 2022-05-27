b13e699d75fadb4d42613d9455ab89d7949614f5
Tom Gregor <tgregor@ivsoftware.com>
2022-05-27 10:44:57 -0600

    Add project files.

---
+++ purchased_items/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace purchased_items
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 33;
+            this.dataGridView1.Size = new System.Drawing.Size(394, 444);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(403, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 62;
+            this.dataGridView2.RowTemplate.Height = 33;
+            this.dataGridView2.Size = new System.Drawing.Size(394, 444);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dataGridView2, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 450);
+            this.tableLayoutPanel1.TabIndex = 1;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "MainForm";
+            this.Text = "Main Form";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ purchased_items/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace purchased_items
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+		}
+
+		protected override void OnHandleCreated(EventArgs e)
+		{
+			base.OnHandleCreated(e);
+			if (!DesignMode)
+			{
+				// Bind the lists to the UI element to display them.
+				dataGridView1.DataSource = PurchasesByUser;
+				dataGridView2.DataSource = PurchasedByItem;
+				initList1();
+			}
+		}
+		internal BindingList<UserPurchase> PurchasesByUser { get; } = new BindingList<UserPurchase>();
+		internal BindingList<PurchasedItem> PurchasedByItem { get; } = new BindingList<PurchasedItem>();
+
+		private void initList1()
+		{
+			XElement xsource = XElement.Parse(source);
+			XElement[] names = xsource.Elements("name").ToArray();
+            foreach (XElement xuser in names)
+            {
+				XElement[] xpurchases = xuser.Elements("item").ToArray();
+                foreach (var xpurchase in xpurchases)
+                {
+					// Every time you add a record to the list, it appears in the View automatically.
+					PurchasesByUser.Add(new UserPurchase(name: (string)xuser, purchase: (string)xpurchase));
+                }
+            }
+			var groups = PurchasesByUser.GroupBy(purchase => purchase.Purchase);
+            foreach (var group in groups)
+            {
+				var byItem = new PurchasedItem(group.Key, group.Count());
+				PurchasedByItem.Add(byItem);
+			}
+		}
+
+        const string source =
+		@"<purchases>
+			<name>John Doe
+				<item>Shoes</item>
+				<item>T-Shirts</item>
+				<item>Jeans</item>
+			</name>
+			<name>
+				Rick Astley
+				<item>Baseball bat</item>
+				<item>Sandwich</item>
+				<item>T-Shirts</item>
+				<item>Shoes</item>
+			</name>
+			<name>
+				Jane Doe
+				<item>Shoes</item>
+				<item>T-Shirts</item>
+			</name>
+			<name>
+				Anonymous
+				<item>Laptop</item>
+			</name>
+			<name>
+				Joe Mm.
+				<item>Milk</item>
+				<item>Shoes</item>
+			</name>
+		</purchases>";
+    }
+	internal class UserPurchase
+	{
+		public UserPurchase(string name, string purchase)
+        {
+			Name = name;
+			Purchase = purchase;
+        }
+		public string Name { get;  }
+		public string Purchase { get;  } 
+	}
+
+
+	internal class PurchasedItem
+    {
+        public PurchasedItem(string description, int count)
+        {
+            Description = description;
+            Count = count;
+        }
+
+        public string Description { get; }
+        public int Count { get; }
+    }
+}
